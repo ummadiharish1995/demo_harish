1d4fcce2612034149c6ac27049f3bb80b1151398
ummadiharish1995 <153350565+ummadiharish1995@users.noreply.github.com>
2023-12-09 16:20:38 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,4 +2,8 @@ public class HelloWorld {
         public static void main(String[] args){
                 System.out.println("Hello World");
                 System.out.println("Welcome to Devops !");
+                int a = 5;
+                int b = 10;
+                int c = a + b;
+                System.out.printin("Sum of two numbers; " + c ");
         }
